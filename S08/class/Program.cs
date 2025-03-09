aea573593bf9207b60bf1d2a288d2ab43faf885c
Sarah <sarahahmdi206@gmail.com>
2025-03-10 00:05:43 +0330

    reversing stuff

---
+++ S08/class/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace S08;
+
+public class Program
+{
+    public static int add(int a ,int b){
+            return a+b;
+        }
+    static void Main(string[] args)
+    {
+        // Console.WriteLine("Hello, World!");
+    }
+}
