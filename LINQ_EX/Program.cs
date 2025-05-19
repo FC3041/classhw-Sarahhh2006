9e4e0298ca0bd43de3676bb5806aa30b04ad0cca
Sarah <sarahahmdi206@gmail.com>
2025-05-19 08:34:51 +0330

    linq hw

--- LINQ_EX/Program.cs
+++ LINQ_EX/Program.cs
@@ -65,26 +65,96 @@ class Program
             .ToList();
 
 
-        foreach (var i in query1) { Console.WriteLine($"({i.Country}, {i.LEType}, {i.Value})"); }
+        foreach (var i in query1) { Console.WriteLine($"({i.Country}, {i.LEType},{i.DataGender}, {i.Value})"); }
         
         Console.WriteLine();
 
         //Query 2
         Console.WriteLine("Query 2");
-        //
-        //
+        double min = query1[0].Value;
+        double max = query1[0].Value;
+        foreach (var n in query1)
+        {
+            if (n.Value < min)
+            {
+                min = n.Value;
+            }
+            else if (n.Value > max)
+            {
+                max = n.Value;
+            }
+        }
+        double dif = max - min;
+        double difference;
+        var query2 = datas
+            .Where(d => d.Country.ToLower().Contains("iran"))
+            .Where(d => d.LEType == LifeExpectancyType.AtBirth)
+            .Where(d => d.DataGender == DataGender.Both)
+            .Select(n =>
+            (n.Country, n.LEType, n.DataGender, difference = dif)
+            )
+            .ToList();
+        foreach (var i in query2) { Console.WriteLine(i); }
         Console.WriteLine();
 
         //Query 3
         Console.WriteLine("Query 3");
-        //
-        //
+        var query3 = datas
+                    .Where(d => d.LEType == LifeExpectancyType.AtBirth)
+                    .Where(d => d.DataGender == DataGender.Both)
+                    .GroupBy(g => g.Country)
+                    .Select(h =>
+                    {
+                        var max = h.MaxBy(l => l.Value);
+                        var min = h.MinBy(l => l.Value);
+                        return (country: h.Key, yearminValue: min.Year, MinValue: min.Value, Diff: max.Value - min.Value);
+
+                    })
+                    .OrderByDescending(s => s.Diff)
+                    .ToList();
+            int rank = 1;
+            foreach (var i in query3) 
+            { 
+                Console.WriteLine($"{rank++}-{i.country},{i.yearminValue},{i.MinValue},{i.Diff}"); 
+            }
+            
+
+                    
+
+        
         Console.WriteLine();
 
         //Query 4
         Console.WriteLine("Query 4");
-        //
-        //
+        var females = datas
+            .Where(d => d.LEType == LifeExpectancyType.AtBirth && d.DataGender == DataGender.Female)
+            .Select(d => new { d.Country, d.Year, femVal = d.Value });
+        var males = datas
+            .Where(d => d.LEType == LifeExpectancyType.AtBirth && d.DataGender == DataGender.Male)
+            .Select(d => new { d.Country, d.Year, manVal = d.Value });
+        var mix = males
+                .Join(females,
+                    male => new { male.Country, male.Year },
+                    female => new { female.Country, female.Year },
+                    (male, female) => new {
+                        male.Country,
+                        male.Year,
+                        male.manVal,
+                        female.femVal,
+                        Diff = Math.Abs(male.manVal- female.femVal)
+                    });
+
+        var maxDiff= mix
+                .GroupBy(d => d.Country)
+                .Select(g => g.OrderByDescending(x => x.Diff).First())
+                .OrderByDescending(x => x.Diff)
+                .ToList();
+
+        rank = 1;
+            foreach (var i in maxDiff) 
+            { 
+                Console.WriteLine($"{rank++}-{i.Country},{i.Year},{i.manVal},{i.femVal} , {i.Diff}"); 
+            }
         Console.WriteLine();
 
     }
--- S21/Program.cs
+++ S21/Program.cs
@@ -46,6 +46,27 @@ class Program
         // }
         // string[] lines =
         File.ReadAllLines("children-per-woman-UN.csv")
+                        .Skip(1)
+                        .Select(l =>
+                        {
+                            string[] tokens = l.Split(",");
+                            string country = tokens[0];
+                            int year = int.Parse(tokens[2]);
+                            double rate = double.Parse(tokens[3]);
+                            return (country: country, year: year, rate: rate);
+                        })
+                        .GroupBy(t => t.country)
+                        //The reason you see System.Linq.Grouping when printing a grouped result
+                        //  in LINQ is because GroupBy() returns an IGrouping<TKey, TElement> collection. Let’s break it down.
+                        .Select(g =>
+                        {
+                            return (country: g.Key, rate: g.Average(t => t.rate));
+                        })
+                        //.Where(t=>(t.year>2012)&&(t.rate>3))
+                        .OrderBy(t=>t.rate)
+                        //.Take(5)
+                        .ToList()
+                        .ForEach(l=>System.Console.WriteLine(l));
 
 
 
