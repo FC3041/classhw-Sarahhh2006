4906d167fd44bd3958d0f99b7da13418baf88018
Sarah <sarahahmdi206@gmail.com>
2025-05-26 22:15:10 +0330

    class codes +HW

--- S20/Class/S20con/Program.cs
+++ S20/Class/S20con/Program.cs
@@ -1,26 +1,51 @@
 ﻿using System.Runtime.Intrinsics.Arm;
 
 namespace S20con;
-public class MyTuple{
-    public MyTuple(){
+// public class MyTuple{
+//     public MyTuple(){
         
+//     }
+// }
+public class MyTuple<T1,T2>
+{
+    public T1 Item1;
+    public T2 Item2;
+
+    public MyTuple(T1 i, T2 j)
+    {
+        this.Item1 = i;
+        this.Item2 = j;
     }
 }
 
 
+
 class Program
 {
-    public static (int , int , double) AnalyzeList(List<int> {
+    // public static (int , int , double) AnalyzeList(List<int> {
 
+    // }
+    public static (int, int , double) AnalyzeList(List<int> numbers)
+    {
+        if(numbers == null || numbers.Count==0)
+            throw new Exception("List must not be empty!");
+        
+        int min = numbers.Min();
+        int max = numbers.Max();
+        double avg = numbers.Average();
+        avg = Math.Round(avg,2);
+
+        return (min, max , avg);
     }
-    static void Main(string[] args){
-        Tuple<string , int> t1 = new Tuple<string , int>("sarah" , 19);
+    static void Main(string[] args)
+    {
+        Tuple<string, int> t1 = new Tuple<string, int>("sarah", 19);
         System.Console.WriteLine(t1.Item1);
-        var t2 = new Tuple<string , int , int>("saba" , 19 , 1234);
+        var t2 = new Tuple<string, int, int>("saba", 19, 1234);
         System.Console.WriteLine(t2.Item1);
         // or we can use Tuple.Create
         //for initializing tuple we can make the class and then use it or use ValueTuple
-        ValueTuple<string , int> t5 = new ValueTuple<string , int>("aryana" , 20);
+        ValueTuple<string, int> t5 = new ValueTuple<string, int>("aryana", 20);
         System.Console.WriteLine(t5.Item1);
         //the last on which is ValueTuple is value type the other ways for initializing the tuple at the first are reference type 
     }
